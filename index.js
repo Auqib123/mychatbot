e538de3cd4cb568f96c92570fc5e59a9c30cd225
auqib <auqib.rather@venturepact.com>
2018-07-06 15:10:15 +0530

    lkdfsj

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const bodyParser=require('body_parser');
+const bodyParser=require('body-parser');
 const request=require('request');
 const app=express();
 // set port tolisten
